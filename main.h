b391f1077469026de244751a327f3f0f161de748
Ryan McDougall <mcdougall.ryan@gmail.com>
2008-03-07 09:18:23 +0000

    add missing header
    git-svn-id: https://voipforvw.svn.sourceforge.net/svnroot/voipforvw@11 cebe1a2a-f746-0410-88f4-ab3133dd3453

---
+++ main.h
@@ -0,0 +1,24 @@
+/* main.h -- exported function header
+ *
+ *			Ryan McDougall -- 20070101
+ */
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <list>
+
+#include <cstdlib>
+#include <cstring>
+
+#include <messaging.hpp>
+#include <sip.hpp>
+#include <state.hpp>
+#include <server.hpp>
+
+using namespace std;
+
+#endif //_MAIN_H_
