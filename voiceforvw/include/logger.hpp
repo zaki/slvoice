cbdfe2267c9b99576c27147034cd741a0c4d6090
Dezso Zoltan <dezso.zoltan@gmail.com>
2009-06-25 12:13:14 +0900

    Adding missing logger files.

---
+++ voiceforvw/include/logger.hpp
@@ -0,0 +1,65 @@
+/* config.h -- exported function header
+ *
+ *			Copyright 2009, 3di.jp Inc
+ */
+
+#ifndef _LOGGER_H_
+#define _LOGGER_H_
+
+//=============================================================================
+// Logger class
+
+enum LogLevels 
+{
+	LL_DEBUG, LL_INFO, LL_WARN, LL_ERROR, LL_FATAL, LL_LEVELCOUNT
+};
+
+static const string LogLevelStrings[] = 
+{
+	"DEBUG", 
+	"INFO", 
+	"WARN", 
+	"ERROR", 
+	"FATAL"
+};
+
+struct nullstream : std::ostream
+{
+	struct nullbuf : std::streambuf 
+	{
+		int overflow(int c) { return traits_type::not_eof(c); }
+	} m_sbuf;
+	nullstream() 
+		: std::ios(&m_sbuf), std::ostream(&m_sbuf) {}
+};
+
+
+class Logger
+{
+    public:
+        Logger ()
+			: logLevel(LL_WARN), outputFilePath("")
+		{};
+
+        ~Logger ();
+
+	public:
+		void Init();
+		void Close();
+		ostream& Log(LogLevels=LL_DEBUG, string section = "");
+
+		ostream& Debug(string section = "");
+		ostream& Info(string section  = "");
+		ostream& Warn(string section  = "");
+		ostream& Error(string section = "");
+		ostream& Fatal(string section = "");
+
+	private:
+		string outputFilePath;
+		std::ofstream file;
+		LogLevels logLevel;
+		string filter;
+		const nullstream ns;
+};
+
+#endif
---
+++ voiceforvw/src/logger/logger.cpp
@@ -0,0 +1,69 @@
+/* logger.cpp -- logger module
+ *
+ *			Copyright 2009, 3di.jp Inc
+ */
+
+#include "main.h"
+#include "logger.hpp"
+#include <time.h>
+#include <iostream>
+#include <fstream>
+
+void Logger::Init()
+{
+	// output file
+	outputFilePath = g_config->LogFilePath;
+	file.open(outputFilePath.c_str(), ios::app);
+
+	// log level
+	for(int i = 0; i < LL_LEVELCOUNT; i++)
+	{
+		if (LogLevelStrings[i] == g_config->LogLevel)
+		{
+			logLevel = (LogLevels)i;
+			break;
+		}
+	}
+}
+
+ostream& Logger::Debug(string section) {return(Log(LL_DEBUG, section));}
+ostream& Logger::Info(string section) {return(Log(LL_INFO, section));}
+ostream& Logger::Warn(string section) {return(Log(LL_WARN, section));}
+ostream& Logger::Error(string section) {return(Log(LL_ERROR, section));}
+ostream& Logger::Fatal(string section) {return(Log(LL_FATAL, section));}
+
+ostream& Logger::Log(LogLevels level, string section)
+{
+	if (level < logLevel)
+	{
+		return((ostream&)ns);
+	}
+
+	if (g_config->LogFilter != "" && section != "")
+	{
+		if (section.find(g_config->LogFilter) == -1)
+			return((ostream&)ns);
+	}
+
+	time_t currTime;
+	struct tm* timeinfo;
+
+	time(&currTime);
+	timeinfo = localtime(&currTime);
+	char timebuffer[20];
+	strftime(timebuffer, 20, "%Y-%m-%d %H:%M:%S", timeinfo);
+	
+	try
+	{
+		file << timebuffer << " ";
+		file << LogLevelStrings[level] << " [" << section << "]: " ;
+	}
+	catch(...) {/* TODO */}
+
+	return(file);
+}
+
+void Logger::Close()
+{
+	file.close();
+}
