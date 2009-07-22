f7924850bb0f2f587dfc7d5813d63d331347ea66
Dezso Zoltan <dezso.zoltan@gmail.com>
2009-07-22 18:19:40 +0900

    Adding event names for easier logging.

--- voiceforvw/include/event.hpp
+++ voiceforvw/include/event.hpp
@@ -51,12 +51,13 @@ enum EventType
 
 struct Event
 {
-	Event() : message(NULL), result(NULL), type(EventType_None) {};
+	Event() : message(NULL), result(NULL), type(EventType_None), event_name("Unknown") {};
 	virtual ~Event() {}
 
 	Request *message;
 	ResponseBase *result;
 	EventType type;
+	string event_name;
 };
 
 struct ConnectorEvent : public Event
@@ -81,18 +82,21 @@ struct SessionEvent : public AccountEvent
 struct InitializeEvent : public ConnectorEvent, event<InitializeEvent> {
 	InitializeEvent() {
 		type = EventType_Initialize;
+		event_name = string("InitializeEvent");
 	};
 };
 
 struct ShutdownEvent : public ConnectorEvent, event<ShutdownEvent> {
 	ShutdownEvent() {
 		type = EventType_Shutdown;
+		event_name = string("ConnectorEvent");
 	};
 };
 
 struct AudioEvent : public ConnectorEvent, event <AudioEvent> {
 	AudioEvent() {
 		type = EventType_Audio;
+		event_name = string("AudioEvent");
 	};
 };
 
@@ -101,6 +105,7 @@ struct AccountLoginEvent : public AccountEvent, event<AccountLoginEvent>
 	AccountLoginEvent() 
 	{
 		type = EventType_AccountLogin;
+		event_name = string("AccountLoginEvent");
 		accountURI = string("");
 	};
 
@@ -110,90 +115,105 @@ struct AccountLoginEvent : public AccountEvent, event<AccountLoginEvent>
 struct AccountLogoutEvent : public AccountEvent, event<AccountLogoutEvent> {
 	AccountLogoutEvent() {
 		type = EventType_AccountLogout;
+		event_name = string("AccountLogoutEvent");
 	};
 };
 
 struct RegSucceedEvent : public AccountEvent, event<RegSucceedEvent> {
 	RegSucceedEvent() {
 		type = EventType_RegSucceed;
+		event_name = string("RegSucceededEvent");
 	};
 };
 
 struct RegFailedEvent : public AccountEvent, event<RegFailedEvent> {
 	RegFailedEvent() {
 		type = EventType_RegFailed;
+		event_name = string("RegFailedEvent");
 	};
 };
 
 struct AccountRemoveEvent : public SessionEvent, event <AccountRemoveEvent> {
 	AccountRemoveEvent() {
 		type = EventType_AccountRemove;
+		event_name = string("AccountRemoveEvent");
 	};
 };
 
 struct SessionCreateEvent : public SessionEvent, event<SessionCreateEvent> {
 	SessionCreateEvent() {
 		type = EventType_SessionCreate;
+		event_name = string("SessionCreateEvent");
 	};
 };
 
 struct SessionTerminateEvent : public SessionEvent, event<SessionTerminateEvent> {
 	SessionTerminateEvent() {
 		type = EventType_SessionTerminate;
+		event_name = string("SessionTerminateEvent");
 	};
 };
 
 struct SessionMediaDisconnectEvent : public SessionEvent, event<SessionMediaDisconnectEvent> {
 	SessionMediaDisconnectEvent() {
 		type = EventType_SessionMediaDisconnect;
+		event_name = string("SessionMediaDisconnectEvent");
 	};
 };
 
 struct SessionConnectEvent : public SessionEvent, event<SessionConnectEvent> {
 	SessionConnectEvent() {
 		type = EventType_SessionConnect;
+		event_name = string("SessionConnectEvent");
 	};
 };
 
 struct DialIncomingEvent : public SessionEvent, event<DialIncomingEvent> {
 	DialIncomingEvent() {
 		type = EventType_DialIncoming;
+		event_name = string("DialIncomingEvent");
 	};
 };
 
 struct DialEarlyEvent : public SessionEvent, event<DialEarlyEvent> {
 	DialEarlyEvent() {
 		type = EventType_DialEarly;
+		event_name = string("DialEarlyEvent");
 	};
 };
 
 struct DialConnectingEvent : public SessionEvent, event<DialConnectingEvent> {
 	DialConnectingEvent() {
 		type = EventType_DialConnecting;
+		event_name = string("DialConnectingEvent");
 	};
 };
 
 struct DialSucceedEvent : public SessionEvent, event<DialSucceedEvent> {
 	DialSucceedEvent() {
 		type = EventType_DialSucceed;
+		event_name = string("DialSucceedEvent");
 	};
 };
 
 struct DialDisconnectedEvent : public SessionEvent, event<DialDisconnectedEvent> {
 	DialDisconnectedEvent() {
 		type = EventType_DialDisconnected;
+		event_name = string("DialDisconnectedEvent");
 	};
 };
 
 struct PositionEvent : public SessionEvent, event <PositionEvent> {
 	PositionEvent() {
 		type = EventType_Position;
+		event_name = string("PositionEvent");
 	};
 };
 
 struct SessionRemoveEvent : public SessionEvent, event <SessionRemoveEvent> {
 	SessionRemoveEvent() {
 		type = EventType_SessionRemove;
+		event_name = string("SessionRemoveEvent");
 	};
 };
 
